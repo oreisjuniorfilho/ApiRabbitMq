5cc715ba307ff64b436b755306f14e71b72dc559
oreisjuniorfilho <138736872+oreisjuniorfilho@users.noreply.github.com>
2024-02-02 22:36:57 -0300

    Adicionar arquivos de projeto.

---
+++ ApiRabbitMq/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiRabbitMq.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRabbitMq.Context;
+
+public class AppDbContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+    public AppDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    public DbSet<Product> Products{get;set;}
+}
---
+++ ApiRabbitMq/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using ApiRabbitMq.Models;
+using ApiRabbitMq.RabbitMQ;
+using ApiRabbitMq.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRabbitMq.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService productService;
+        private readonly IRabbitMQProducer _rabitMQProducer;
+        public ProductsController(IProductService _productService,
+                                  IRabbitMQProducer rabitMQProducer)
+        {
+            productService = _productService;
+            _rabitMQProducer = rabitMQProducer;
+        }
+        [HttpGet("productlist")]
+        public IEnumerable<Product> ProductList()
+        {
+            var productList = productService.GetProductList();
+            return productList;
+        }
+        [HttpGet("getproductbyid")]
+        public Product GetProductById(int Id)
+        {
+            return productService.GetProductById(Id);
+        }
+        [HttpPost("addproduct")]
+        public Product AddProduct(Product product)
+        {
+            var productData = productService.AddProduct(product);
+            //envia os dados do produto para a fila e o consumer
+            //vai ouvir os dados a partir da fila
+            _rabitMQProducer.SendProductMessage(productData);
+            return productData;
+        }
+        [HttpPut("updateproduct")]
+        public Product UpdateProduct(Product product)
+        {
+            return productService.UpdateProduct(product);
+        }
+        [HttpDelete("deleteproduct")]
+        public bool DeleteProduct(int Id)
+        {
+            return productService.DeleteProduct(Id);
+        }
+    }
+}
